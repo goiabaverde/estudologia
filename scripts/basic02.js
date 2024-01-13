6c1bcd2950e9ccd89bc0ccba72446d1d7a51ae79
goiabaverde <120653207+goiabaverde@users.noreply.github.com>
2024-01-13 14:58:25 -0300

    Update basic02.js

--- scripts/basic02.js
+++ scripts/basic02.js
@@ -8,11 +8,5 @@ try{
 function change_width(event_target){
     console.log("TEST")
     let new_width = (event_target.value.length * 8)  + 40;
-    new_width = (event_target.textContent.length * 8) + 40;
     event_target.style.width = `${new_width}px`        
 }
-
-
-
-         
-
